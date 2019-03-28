2bc33a4186da42b56aa7d9b16c46d2116e961f39
Nosyash <kkomi.komii@gmail.com>
2019-03-28 18:35:53 +0300

    fix ws_endpoint at client

--- client/src/Components/Room/chat/ChatInner.jsx
+++ client/src/Components/Room/chat/ChatInner.jsx
@@ -34,7 +34,7 @@ class ChatInner_ extends Component {
   }
 
   webSocketConnect = () => {
-    const { REACT_APP_SOCKET_ENDPOINT } = process.env;  
+    const { REACT_APP_SOCKET_ENDPOINT } = process.env;
     this.socket = new WebSocket(REACT_APP_SOCKET_ENDPOINT);
   };
 
