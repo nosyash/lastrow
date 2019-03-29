83b8d408a9e2da16d0cd3c93236aca6d9eac540a
Nosyash <kkomi.komii@gmail.com>
2019-03-29 17:49:34 +0300

    one more time

--- client/src/Components/Room/Base.jsx
+++ client/src/Components/Room/Base.jsx
@@ -81,7 +81,7 @@ class RoomBase_ extends Component {
     const { open, connected } = this.state;
     const { REACT_APP_SOCKET_ENDPOINT: socket } = process.env;
     if (open || connected) return;
-    if (socket) this.socket = new WebSocket(server);
+    if (socket) this.socket = new WebSocket(socket);
     if (!socket) console.error('No WebSocket address was provided');
     this.setState({ open: true });
     this.initWebSocketEvents();
