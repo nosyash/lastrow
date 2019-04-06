df22b61db3c3684018286cb7e10010febe9de8f9
nunuju <38221096+nunuju@users.noreply.github.com>
2019-04-06 18:22:47 +0300

    close popup on background click

--- client/src/Components/UI/FloatElements.jsx
+++ client/src/Components/UI/FloatElements.jsx
@@ -1,23 +1,39 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { getCenteredRect } from '../../utils/base';
+import * as types from '../../constants/ActionTypes';
 
-const FloatElements = ({ components }) => (
+const FloatElements = ({ components, removeFloat }) => (
   <div className="float-elements">
     {components.map((el, i) => (
       <div
-        className="float-element_contaiter"
         key={i}
-        style={{ ...getCenteredRect(el.width, el.height) }}
+        onMouseDown={e => handleClose(e, el.id, removeFloat)}
+        className="close-area"
       >
-        {el.el}
+        <div
+          className="float-element_contaiter"
+          style={{ ...getCenteredRect(el.width, el.height) }}
+        >
+          {el.el}
+        </div>
       </div>
     ))}
   </div>
 );
 
+const handleClose = (e, id, removeFloat) =>
+  e.target.matches('.close-area') ? removeFloat(id) : null;
+
 const mapStateToProps = state => ({
   components: state.Components.list,
 });
 
-export default connect(mapStateToProps)(FloatElements);
+const mapDispatchToProps = {
+  removeFloat: payload => ({ type: types.REMOVE_COMPONENT, payload }),
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(FloatElements);
