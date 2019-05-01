79ebba8159d93f6996eddac3ec4c9e13bd9c4b1e
nunuju <38221096+nunuju@users.noreply.github.com>
2019-05-02 00:46:04 +0300

    remove unused code

--- client/src/Components/RoomList/RoomItem.jsx
+++ client/src/Components/RoomList/RoomItem.jsx
@@ -4,23 +4,6 @@ import { Link } from 'react-router-dom';
 class RoomItem extends Component {
   state = {};
 
-  // render() {
-  //   const { title, movie, users, link } = this.props;
-  //   return (
-  //     <Link to={link} className="room-item">
-  //       <h2 className="room-item_title">{title}</h2>
-  //       <p className="room-item_movie">
-  //         <i className="fa fa-film" />
-  //         {movie}
-  //       </p>
-  //       <p className="room-item_users">
-  //         <i className="fa fa-user" />
-  //         {users}
-  //       </p>
-  //     </Link>
-  //   );
-  // }
-
   render() {
     const { title, movie, users, link } = this.props;
     console.log(this.props);
