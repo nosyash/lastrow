b95538f1300a1c6209015f9f517ccc58baa58313
bonanov <38221096+bonanov@users.noreply.github.com>
2019-08-27 17:31:05 +0300

    Use hooks in ChatContainer

--- client/src/Components/Room/chat/ChatContainer.jsx
+++ client/src/Components/Room/chat/ChatContainer.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { RESIZER_SEL, CHAT_HEADER_SEL, DIVIDER_SEL } from '../../../constants';
@@ -12,105 +12,106 @@ import ChatInner from './ChatInner';
 import { UPDATE_MAIN_STATES } from '../../../constants/ActionTypes';
 import ControlPanel from '../../UI/ControlPanel';
 
-class ChatContainer extends Component {
-  constructor() {
-    super();
-    this.left = 0;
-    this.top = 0;
-    this.baseX = 0;
-    this.baseY = 0;
-    this.resizing = false;
-    this.moving = false;
-  }
+let left = 0;
+let top = 0;
+let baseX = 0;
+let baseY = 0;
+let resizing = false;
+let moving = false;
 
-  state = {
-    width: 290,
-    height: 490,
-    top: 50,
-    left: 50,
-  };
-
-  componentDidMount() {
+function ChatContainer(props) {
+  const [width, setWidth] = useState(290);
+  const [height, setHeight] = useState(490);
+  const [top, setTop] = useState(50);
+  const [left, setLeft] = useState(50);
+  useEffect(() => {
     if (localStorage.chat) {
       const { chat } = localStorage;
       const chatParams = JSON.parse(chat);
       const { width, height, left, top } = chatParams;
 
-      const params = {
+      const coordinates = {
         width: width || 300,
         height: height || 500,
         left: left || 50,
         top: top || 50,
       };
 
-      this.setState({ ...params });
+      setStates(coordinates);
     }
 
     document.addEventListener('mousedown', this.handleGlobalMouseDown);
     document.addEventListener('mousemove', this.handleGlobalMouseMove);
     document.addEventListener('mouseup', this.handleGlobalMouseUp);
+
+    return () => {
+      document.removeEventListener('mousedown', handleGlobalMouseDown);
+      document.removeEventListener('mousemove', handleGlobalMouseMove);
+      document.removeEventListener('mouseup', handleGlobalMouseUp);
+
+    }
+  }, [])
+
+  function setStates({ width, height, top, left }) {
+    if (width) setWidth(width)
+    if (height) setHeight(height)
+    if (top) setTop(top)
+    if (left) setLeft(left);
   }
 
-  componentWillUnmount() {
-    document.removeEventListener('mousedown', this.handleGlobalMouseDown);
-    document.removeEventListener('mousemove', this.handleGlobalMouseMove);
-    document.removeEventListener('mouseup', this.handleGlobalMouseUp);
-  }
-
-  handleGlobalMouseDown = e => {
-    const { cinemaMode } = this.props;
+  function handleGlobalMouseDown(e) {
+    const { cinemaMode } = props;
     const { target } = e;
     if (target.closest(RESIZER_SEL)) {
-      this.handleResizerDown(e, { resizer: true });
+      handleResizerDown(e, { resizer: true });
     }
     if (target.closest(DIVIDER_SEL)) {
       if (!cinemaMode) {
-        this.handleResizerDown(e, { divider: true });
+        handleResizerDown(e, { divider: true });
       }
     }
     if (target.closest(CHAT_HEADER_SEL)) {
       if (cinemaMode) {
-        this.handleHeaderDown(e);
+        handleHeaderDown(e);
       }
     }
   };
 
-  handleGlobalMouseMove = e => {
-    const { chat, cinemaMode } = this.props;
+  function handleGlobalMouseMove(e) {
+    const { chat, cinemaMode } = props;
 
     if (!chat.current) return;
-    if (this.resizing && cinemaMode) this.handleResize(e, { resizer: true });
-    if (this.resizing && !cinemaMode) this.handleResize(e, { divider: true });
-    if (this.moving) this.handleMove(e);
+    if (resizing && cinemaMode) handleResize(e, { resizer: true });
+    if (resizing && !cinemaMode) handleResize(e, { divider: true });
+    if (moving) handleMove(e);
   };
 
-  handleGlobalMouseUp = () => {
-    if (!this.resizing && !this.moving) return;
-    const { video } = this.props;
-    const { width, height, left, top } = this.state;
+  function handleGlobalMouseUp() {
+    if (!resizing && !moving) return;
+    const { video } = props;
 
     const chatParams = JSON.stringify({ width, height, left, top });
     localStorage.chat = chatParams;
 
-    this.resizing = false;
-    this.moving = false;
+    resizing = false;
+    moving = false;
     toggleUserSelect();
     unsetCursorStyle();
     togglePointerEvent(video.current);
   };
 
-  handleResizerDown = (e, { resizer, divider }) => {
-    const { video, chat } = this.props;
-    if (this.resizing) return;
-    this.resizing = true;
+  function handleResizerDown(e, { resizer, divider }) {
+    const { video, chat } = props;
+    if (resizing) return;
+    resizing = true;
     const rect = chat.current.getBoundingClientRect();
-    this.left = rect.left;
-    this.top = rect.top;
+    left = rect.left;
+    top = rect.top;
     const offset = resizer ? 10 : -5;
-    const width = e.clientX - this.left + offset;
-    const height = e.clientY - this.top + offset;
-    if (resizer) this.setState({ width, height });
-    if (divider) this.setState({ width });
+    const width = e.clientX - left + offset;
+    const height = e.clientY - top + offset;
+    if (resizer) setState({ width, height });
+    if (divider) setState({ width });
 
     toggleUserSelect();
     if (resizer) toggleCursor('se-resize');
@@ -118,33 +119,32 @@ class ChatContainer extends Component {
     togglePointerEvent(video.current);
   };
 
-  handleHeaderDown = e => {
-    const { video, chat } = this.props;
-    if (this.moving) return;
-    this.moving = true;
+  function handleHeaderDown(e) {
+    const { video, chat } = props;
+    if (moving) return;
+    moving = true;
     const rect = chat.current.getBoundingClientRect();
     const { clientX, clientY } = e;
-    this.baseX = clientX;
-    this.baseY = clientY;
-    this.left = rect.left;
-    this.top = rect.top;
-    const left = this.left + clientX - this.baseX;
-    const top = this.top + clientY - this.baseY;
-    this.setState({ left, top });
+    baseX = clientX;
+    baseY = clientY;
+    left = rect.left;
+    top = rect.top;
+    const left = left + clientX - baseX;
+    const top = top + clientY - baseY;
+    setStates({ left, top });
 
     toggleUserSelect();
     toggleCursor('move');
     togglePointerEvent(video.current);
   };
 
-  handleResize = (e, { resizer, divider }) => {
-    let { chat } = this.props;
+  function handleResize(e, { resizer, divider }) {
+    let { chat } = props;
     chat = chat.current;
 
-    let { height, width } = this.state;
     const offset = resizer ? 10 : -5;
-    width = e.clientX - this.left + offset;
-    height = e.clientY - this.top + offset;
+    width = e.clientX - left + offset;
+    height = e.clientY - top + offset;
 
     if (width < 30) {
       chat.classList.add('collapsed');
@@ -152,46 +152,44 @@ class ChatContainer extends Component {
       chat.classList.remove('collapsed');
     }
 
-    if (resizer) this.setState({ width, height });
-    if (divider) this.setState({ width });
+    if (resizer) setStates({ width, height });
+    if (divider) setStates({ width });
   };
 
-  handleMove = e => {
-    if (!this.moving) return;
+  function handleMove(e) {
+    if (!moving) return;
     const { clientX, clientY } = e;
-    const left = this.left + clientX - this.baseX;
-    const top = this.top + clientY - this.baseY;
+    const left = left + clientX - baseX;
+    const top = top + clientY - baseY;
 
-    this.setState({ left, top });
+    setStates({ left, top });
   };
 
-  render() {
-    const { chat, cinemaMode, socket } = this.props;
-    const { width, top, left } = this.state;
-    let { height } = this.state;
-    height = cinemaMode ? height : '';
-    const position = cinemaMode ? 'fixed' : '';
-    const className = cinemaMode ? 'cinema-mode' : '';
-    return (
-      <div
-        ref={chat}
-        style={{ width, height, left, top, position }}
-        className={`chat-container ${className}`}
-      >
-        {cinemaMode && (
-          <span className="resizer">
-            <i className="fa fa-angle-down" />
-          </span>
-        )}
-        <Link to="/" className="control go-back">
-          <i className="fa fa-arrow-left" />
-          {' Back to rooms'}
-        </Link>
-        <ChatInner socket={socket} />
-        <ControlPanel />
-      </div>
-    );
-  }
+  const { chat, cinemaMode, socket } = props;
+  let newHeight = height;
+  newHeight = cinemaMode ? newHeight : '';
+  const position = cinemaMode ? 'fixed' : '';
+  const className = cinemaMode ? 'cinema-mode' : '';
+
+  return (
+    <div
+      ref={chat}
+      style={{ width, height: newHeight, left, top, position }}
+      className={`chat-container ${className}`}
+    >
+      {cinemaMode && (
+        <span className="resizer">
+          <i className="fa fa-angle-down" />
+        </span>
+      )}
+      <Link to="/" className="control go-back">
+        <i className="fa fa-arrow-left" />
+        {' Back to rooms'}
+      </Link>
+      <ChatInner socket={socket} />
+      <ControlPanel />
+    </div>
+  );
 }
 
 const mapDispatchToProps = {
