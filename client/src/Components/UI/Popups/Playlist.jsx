865027fbf20a43e731c77d523b2ea4fc0b261193
bonanov <38221096+bonanov@users.noreply.github.com>
2019-08-31 09:44:43 +0300

    add url fallback to playlist items name

--- client/src/Components/UI/Popups/Playlist.jsx
+++ client/src/Components/UI/Popups/Playlist.jsx
@@ -7,13 +7,8 @@ class Playlist extends Component {
 
   renderElement = (element, i) => (
     <div key={i} className="paylist-item">
-      <a
-        className="control"
-        target="_blank"
-        rel="noopener noreferrer"
-        href={element.url}
-      >
-        {element.title}
+      <a className="control" target="_blank" rel="noopener noreferrer" href={element.url}>
+        {element.title || element.url}
       </a>
     </div>
   );
