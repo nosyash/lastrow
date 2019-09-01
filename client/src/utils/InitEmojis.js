0e1317c4728274f830a882e005cba6dafbcc2def
bonanov <38221096+bonanov@users.noreply.github.com>
2019-09-01 18:50:15 +0300

    copy emotes from IreneTheatre

--- client/src/utils/InitEmojis.js
+++ client/src/utils/InitEmojis.js
@@ -1,8 +1,439 @@
 const getEmojiList = () => [
   {
+    name: 'arin_smile',
+    url:
+      'https://github.com/cutechan/cutechan/blob/master/smiles/arin_smile.png?raw=true',
+    url2x:
+      'https://github.com/cutechan/cutechan/blob/master/smiles/arin_smile.png?raw=true',
+  },
+  {
+    name: 'autism',
+    url: 'https://github.com/cutechan/cutechan/blob/master/smiles/autism.png?raw=true',
+    url2x: 'https://github.com/cutechan/cutechan/blob/master/smiles/autism.png?raw=true',
+  },
+  {
+    name: 'beast',
+    url: 'https://github.com/cutechan/cutechan/blob/master/smiles/beast.png?raw=true',
+    url2x: 'https://github.com/cutechan/cutechan/blob/master/smiles/beast.png?raw=true',
+  },
+  {
+    name: 'bin_happy',
+    url: 'https://github.com/cutechan/cutechan/blob/master/smiles/bin_happy.png?raw=true',
+    url2x:
+      'https://github.com/cutechan/cutechan/blob/master/smiles/bin_happy.png?raw=true',
+  },
+  {
+    name: 'bin_supersad',
+    url:
+      'https://github.com/cutechan/cutechan/blob/master/smiles/bin_supersad.png?raw=true',
+    url2x:
+      'https://github.com/cutechan/cutechan/blob/master/smiles/bin_supersad.png?raw=true',
+  },
+  {
+    name: 'bona_digust',
+    url:
+      'https://raw.githubusercontent.com/cutechan/cutechan/master/smiles/bona_disgust.png',
+    url2x:
+      'https://raw.githubusercontent.com/cutechan/cutechan/master/smiles/bona_disgust.png',
+  },
+  {
     name: 'bona_sad',
-    url: 'https://files.catbox.moe/iq25ih.png',
-    url2x: 'https://files.catbox.moe/igwn7x.png',
+    url: 'https://i.imgur.com/p2VgE66.png',
+    url2x: 'https://i.imgur.com/p2VgE66.png',
+  },
+  {
+    name: 'cha_hnng',
+    url: 'https://github.com/cutechan/cutechan/blob/master/smiles/cha_hnng.png?raw=true',
+    url2x:
+      'https://github.com/cutechan/cutechan/blob/master/smiles/cha_hnng.png?raw=true',
+  },
+  {
+    name: 'cha_wtf',
+    url: 'https://github.com/cutechan/cutechan/blob/master/smiles/cha_wut.png?raw=true',
+    url2x: 'https://github.com/cutechan/cutechan/blob/master/smiles/cha_wut.png?raw=true',
+  },
+  {
+    name: 'chen_lol',
+    url: 'https://github.com/cutechan/cutechan/blob/master/smiles/chen_lol.png?raw=true',
+    url2x:
+      'https://github.com/cutechan/cutechan/blob/master/smiles/chen_lol.png?raw=true',
+  },
+  {
+    name: 'chen_sulk',
+    url: 'https://github.com/cutechan/cutechan/blob/master/smiles/chen_sulk.png?raw=true',
+    url2x:
+      'https://github.com/cutechan/cutechan/blob/master/smiles/chen_sulk.png?raw=true',
+  },
+  {
+    name: 'cigarette',
+    url: 'https://github.com/cutechan/cutechan/blob/master/smiles/cigarette.png?raw=true',
+    url2x:
+      'https://github.com/cutechan/cutechan/blob/master/smiles/cigarette.png?raw=true',
+  },
+  {
+    name: 'cool',
+    url: 'https://github.com/cutechan/cutechan/blob/master/smiles/cool.png?raw=true',
+    url2x: 'https://github.com/cutechan/cutechan/blob/master/smiles/cool.png?raw=true',
+  },
+  {
+    name: 'dahyun_dab',
+    url:
+      'https://github.com/cutechan/cutechan/blob/master/smiles/dahyun_dab.png?raw=true',
+    url2x:
+      'https://github.com/cutechan/cutechan/blob/master/smiles/dahyun_dab.png?raw=true',
+  },
+  {
+    name: 'dahyun_scream',
+    url: 'https://i.imgur.com/vIr3OkQ.gif',
+    url2x: 'https://i.imgur.com/vIr3OkQ.gif',
+  },
+  {
+    name: 'dance2',
+    url: 'https://i.imgur.com/82BG03O.gif',
+    url2x: 'https://i.imgur.com/82BG03O.gif',
+  },
+  {
+    name: 'doyeon_lol',
+    url:
+      'https://github.com/cutechan/cutechan/blob/master/smiles/doyeon_lol.png?raw=true',
+    url2x:
+      'https://github.com/cutechan/cutechan/blob/master/smiles/doyeon_lol.png?raw=true',
+  },
+  {
+    name: 'eunbin_sulk',
+    url:
+      'https://github.com/cutechan/cutechan/blob/master/smiles/eunbin_sulk.png?raw=true',
+    url2x:
+      'https://github.com/cutechan/cutechan/blob/master/smiles/eunbin_sulk.png?raw=true',
+  },
+  {
+    name: 'eunbin_wut',
+    url:
+      'https://github.com/cutechan/cutechan/blob/master/smiles/eunbin_wut.png?raw=true',
+    url2x:
+      'https://github.com/cutechan/cutechan/blob/master/smiles/eunbin_wut.png?raw=true',
+  },
+  {
+    name: 'euns_angry',
+    url:
+      'https://github.com/cutechan/cutechan/blob/master/smiles/euns_angry.png?raw=true',
+    url2x:
+      'https://github.com/cutechan/cutechan/blob/master/smiles/euns_angry.png?raw=true',
+  },
+  {
+    name: 'exy_tired',
+    url: 'https://github.com/cutechan/cutechan/blob/master/smiles/exy_tired.png?raw=true',
+    url2x:
+      'https://github.com/cutechan/cutechan/blob/master/smiles/exy_tired.png?raw=true',
+  },
+  {
+    name: 'fany_smile',
+    url: 'https://i.imgur.com/STjTFx8.png',
+    url2x: 'https://i.imgur.com/STjTFx8.png',
+  },
+  {
+    name: 'fany_ugh',
+    url: 'https://i.imgur.com/VNJZFvt.png',
+    url2x: 'https://i.imgur.com/VNJZFvt.png',
+  },
+  {
+    name: 'gay',
+    url: 'https://i.imgur.com/wuRT6e3.png',
+    url2x: 'https://i.imgur.com/wuRT6e3.png',
+  },
+  {
+    name: 'gowoon_scream',
+    url: 'https://i.imgur.com/4jjqyux.png',
+    url2x: 'https://i.imgur.com/4jjqyux.png',
+  },
+  {
+    name: 'heart4',
+    url: 'https://github.com/cutechan/cutechan/blob/master/smiles/heart4.png?raw=true',
+    url2x: 'https://github.com/cutechan/cutechan/blob/master/smiles/heart4.png?raw=true',
+  },
+  {
+    name: 'heart',
+    url: 'https://github.com/cutechan/cutechan/blob/master/smiles/heart.png?raw=true',
+    url2x: 'https://github.com/cutechan/cutechan/blob/master/smiles/heart.png?raw=true',
+  },
+  {
+    name: 'hyperlol',
+    url: 'https://github.com/cutechan/cutechan/blob/master/smiles/hyperlol.png?raw=true',
+    url2x:
+      'https://github.com/cutechan/cutechan/blob/master/smiles/hyperlol.png?raw=true',
+  },
+  {
+    name: 'irene_wut',
+    url: 'https://i.imgur.com/ltOhLYx.png',
+    url2x: 'https://i.imgur.com/ltOhLYx.png',
+  },
+  {
+    name: 'jennie_sad',
+    url: 'https://i.imgur.com/LXn0bFN.png',
+    url2x: 'https://i.imgur.com/LXn0bFN.png',
+  },
+  {
+    name: 'jeongyeon_smile',
+    url: 'https://i.imgur.com/fDvfacF.png',
+    url2x: 'https://i.imgur.com/fDvfacF.png',
+  },
+  {
+    name: 'joker',
+    url: 'https://i.imgur.com/V5QPDIV.png',
+    url2x: 'https://i.imgur.com/V5QPDIV.png',
+  },
+  {
+    name: 'joy_oh',
+    url: 'https://i.imgur.com/B5SjDsm.png',
+    url2x: 'https://i.imgur.com/B5SjDsm.png',
+  },
+  {
+    name: 'joy_think',
+    url: 'https://raw.githubusercontent.com/nunuju/Emotes/master/pngs/joy_think.png',
+    url2x: 'https://raw.githubusercontent.com/nunuju/Emotes/master/pngs/joy_think.png',
+  },
+  {
+    name: 'kyul_grr',
+    url: 'https://github.com/cutechan/cutechan/blob/master/smiles/kyul_grr.png?raw=true',
+    url2x:
+      'https://github.com/cutechan/cutechan/blob/master/smiles/kyul_grr.png?raw=true',
+  },
+  {
+    name: 'kyul_susp',
+    url: 'https://github.com/cutechan/cutechan/blob/master/smiles/kyul_susp.png?raw=true',
+    url2x:
+      'https://github.com/cutechan/cutechan/blob/master/smiles/kyul_susp.png?raw=true',
+  },
+  {
+    name: 'luda_angry',
+    url:
+      'https://github.com/cutechan/cutechan/blob/master/smiles/luda_angry.png?raw=true',
+    url2x:
+      'https://github.com/cutechan/cutechan/blob/master/smiles/luda_angry.png?raw=true',
+  },
+  {
+    name: 'luda_lol',
+    url: 'https://github.com/cutechan/cutechan/blob/master/smiles/luda_lol.png?raw=true',
+    url2x:
+      'https://github.com/cutechan/cutechan/blob/master/smiles/luda_lol.png?raw=true',
+  },
+  {
+    name: 'mijoo_boring',
+    url:
+      'https://github.com/cutechan/cutechan/blob/master/smiles/mijoo_boring.png?raw=true',
+    url2x:
+      'https://github.com/cutechan/cutechan/blob/master/smiles/mijoo_boring.png?raw=true',
+  },
+  {
+    name: 'minaoa_sad',
+    url:
+      'https://github.com/cutechan/cutechan/blob/master/smiles/minaoa_sad.png?raw=true',
+    url2x:
+      'https://github.com/cutechan/cutechan/blob/master/smiles/minaoa_sad.png?raw=true',
+  },
+  {
+    name: 'nancy_disgust',
+    url: 'https://raw.githubusercontent.com/nunuju/Emotes/master/pngs/nancy_disgust.png',
+    url2x:
+      'https://raw.githubusercontent.com/nunuju/Emotes/master/pngs/nancy_disgust.png',
+  },
+  {
+    name: 'nancy_smile',
+    url:
+      'https://github.com/cutechan/cutechan/blob/master/smiles/nancy_smile.png?raw=true',
+    url2x:
+      'https://github.com/cutechan/cutechan/blob/master/smiles/nancy_smile.png?raw=true',
+  },
+  {
+    name: 'nay_scream',
+    url:
+      'https://github.com/cutechan/cutechan/blob/master/smiles/nay_scream.png?raw=true',
+    url2x:
+      'https://github.com/cutechan/cutechan/blob/master/smiles/nay_scream.png?raw=true',
+  },
+  {
+    name: 'nay_susp',
+    url: 'https://github.com/cutechan/cutechan/blob/master/smiles/nay_susp.png?raw=true',
+    url2x:
+      'https://github.com/cutechan/cutechan/blob/master/smiles/nay_susp.png?raw=true',
+  },
+  {
+    name: 'nosick:',
+    url: 'https://i.imgur.com/VtAnv6e.png',
+    url2x: 'https://i.imgur.com/VtAnv6e.png',
+  },
+  {
+    name: 'nunu_angry',
+    url:
+      'https://github.com/cutechan/cutechan/blob/master/smiles/nunu_angry.png?raw=true',
+    url2x:
+      'https://github.com/cutechan/cutechan/blob/master/smiles/nunu_angry.png?raw=true',
+  },
+  {
+    name: 'nunu_anime',
+    url: 'https://i.imgur.com/4MZLNxP.png',
+    url2x: 'https://i.imgur.com/4MZLNxP.png',
+  },
+  {
+    name: 'nunu_clown',
+    url: 'https://i.imgur.com/GsItesP.png',
+    url2x: 'https://i.imgur.com/GsItesP.png',
+  },
+  {
+    name: 'nunu_cool',
+    url: 'https://i.imgur.com/6Yxu3Cf.png',
+    url2x: 'https://i.imgur.com/6Yxu3Cf.png',
+  },
+  {
+    name: 'nunu_cry',
+    url: 'https://github.com/cutechan/cutechan/blob/master/smiles/nunu_cry.png?raw=true',
+    url2x:
+      'https://github.com/cutechan/cutechan/blob/master/smiles/nunu_cry.png?raw=true',
+  },
+  {
+    name: 'nunu_dance',
+    url: 'https://i.imgur.com/4tjMPzZ.gif',
+    url2x: 'https://i.imgur.com/4tjMPzZ.gif',
+  },
+  {
+    name: 'nunu_grr',
+    url: 'https://github.com/cutechan/cutechan/blob/master/smiles/nunu_grr.png?raw=true',
+    url2x:
+      'https://github.com/cutechan/cutechan/blob/master/smiles/nunu_grr.png?raw=true',
+  },
+  {
+    name: 'nunu_hop',
+    url: 'https://i.imgur.com/8sm9mqh.gif',
+    url2x: 'https://i.imgur.com/8sm9mqh.gif',
+  },
+  {
+    name: 'nunu_judge',
+    url: 'https://i.imgur.com/0Jex21k.gif',
+    url2x: 'https://i.imgur.com/0Jex21k.gif',
+  },
+  {
+    name: 'nunu_lol',
+    url: 'https://github.com/cutechan/cutechan/blob/master/smiles/nunu_lol.png?raw=true',
+    url2x:
+      'https://github.com/cutechan/cutechan/blob/master/smiles/nunu_lol.png?raw=true',
+  },
+  {
+    name: 'nunu_palm',
+    url: 'https://github.com/cutechan/cutechan/blob/master/smiles/nunu_palm.png?raw=true',
+    url2x:
+      'https://github.com/cutechan/cutechan/blob/master/smiles/nunu_palm.png?raw=true',
+  },
+  {
+    name: 'nunu_pot',
+    url: 'https://i.imgur.com/89a3uW5.png',
+    url2x: 'https://i.imgur.com/89a3uW5.png',
+  },
+  {
+    name: 'nunu_smart',
+    url:
+      'https://github.com/cutechan/cutechan/blob/master/smiles/nunu_smart.png?raw=true',
+    url2x:
+      'https://github.com/cutechan/cutechan/blob/master/smiles/nunu_smart.png?raw=true',
+  },
+  {
+    name: 'nunu_spy',
+    url: 'https://i.imgur.com/qQgwxW6.png',
+    url2x: 'https://i.imgur.com/qQgwxW6.png',
+  },
+  {
+    name: 'nunu_susp',
+    url: 'https://raw.githubusercontent.com/nunuju/Emotes/master/pngs_gulp/nunu_susp.png',
+    url2x:
+      'https://raw.githubusercontent.com/nunuju/Emotes/master/pngs_gulp/nunu_susp.png',
+  },
+  {
+    name: 'nunu_wut2',
+    url: 'https://github.com/cutechan/cutechan/blob/master/smiles/nunu_wut.png?raw=true',
+    url2x:
+      'https://github.com/cutechan/cutechan/blob/master/smiles/nunu_wut.png?raw=true',
+  },
+  {
+    name: 'orly',
+    url: 'https://github.com/cutechan/cutechan/blob/master/smiles/orly.png?raw=true',
+    url2x: 'https://github.com/cutechan/cutechan/blob/master/smiles/orly.png?raw=true',
+  },
+  {
+    name: 'roa_cry',
+    url: 'https://github.com/cutechan/cutechan/blob/master/smiles/roa_cry.png?raw=true',
+    url2x: 'https://github.com/cutechan/cutechan/blob/master/smiles/roa_cry.png?raw=true',
+  },
+  {
+    name: 'sad',
+    url: 'https://i.imgur.com/QKVMTdX.png',
+    url2x: 'https://i.imgur.com/QKVMTdX.png',
+  },
+  {
+    name: 'shy',
+    url: 'https://github.com/cutechan/cutechan/blob/master/smiles/shy.png?raw=true',
+    url2x: 'https://github.com/cutechan/cutechan/blob/master/smiles/shy.png?raw=true',
+  },
+  {
+    name: 'som_why',
+    url: 'https://github.com/cutechan/cutechan/blob/master/smiles/som_why.png?raw=true',
+    url2x: 'https://github.com/cutechan/cutechan/blob/master/smiles/som_why.png?raw=true',
+  },
+  {
+    name: 'som_wut',
+    url: 'https://github.com/cutechan/cutechan/blob/master/smiles/som_wut.png?raw=true',
+    url2x: 'https://github.com/cutechan/cutechan/blob/master/smiles/som_wut.png?raw=true',
+  },
+  {
+    name: 'tae_welp',
+    url: 'https://github.com/cutechan/cutechan/blob/master/smiles/tae_welp.png?raw=true',
+    url2x:
+      'https://github.com/cutechan/cutechan/blob/master/smiles/tae_welp.png?raw=true',
+  },
+  {
+    name: 'tellmemore',
+    url:
+      'https://github.com/cutechan/cutechan/blob/master/smiles/tellmemore.png?raw=true',
+    url2x:
+      'https://github.com/cutechan/cutechan/blob/master/smiles/tellmemore.png?raw=true',
+  },
+  {
+    name: 'wend_wow',
+    url: 'https://i.imgur.com/F0En9T6.gif',
+    url2x: 'https://i.imgur.com/F0En9T6.gif',
+  },
+  {
+    name: 'wine',
+    url: 'https://github.com/cutechan/cutechan/blob/master/smiles/wine.png?raw=true',
+    url2x: 'https://github.com/cutechan/cutechan/blob/master/smiles/wine.png?raw=true',
+  },
+  {
+    name: 'yein_shocked',
+    url: 'https://i.imgur.com/Yh2E1tL.gif',
+    url2x: 'https://i.imgur.com/Yh2E1tL.gif',
+  },
+  {
+    name: 'yeojin',
+    url: 'https://i.imgur.com/IJb3f9G.png',
+    url2x: 'https://i.imgur.com/IJb3f9G.png',
+  },
+  {
+    name: 'yeonwoo_smile',
+    url:
+      'https://github.com/cutechan/cutechan/blob/master/smiles/yeonwoo_smile.png?raw=true',
+    url2x:
+      'https://github.com/cutechan/cutechan/blob/master/smiles/yeonwoo_smile.png?raw=true',
+  },
+  {
+    name: 'yeri_angry',
+    url:
+      'https://github.com/cutechan/cutechan/blob/master/smiles/yeri_angry.png?raw=true',
+    url2x:
+      'https://github.com/cutechan/cutechan/blob/master/smiles/yeri_angry.png?raw=true',
+  },
+  {
+    name: 'yuna_sad',
+    url: 'https://github.com/cutechan/cutechan/blob/master/smiles/yuna_sulk.png?raw=true',
+    url2x:
+      'https://github.com/cutechan/cutechan/blob/master/smiles/yuna_sulk.png?raw=true',
   },
 ];
 
