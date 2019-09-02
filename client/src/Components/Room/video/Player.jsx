6439d172ad9d75cd3e47f0846b2eb93c541fc6c0
bonanov <38221096+bonanov@users.noreply.github.com>
2019-09-02 20:38:27 +0300

    reduce maximum video sync offset

--- client/src/Components/Room/video/Player.jsx
+++ client/src/Components/Room/video/Player.jsx
@@ -5,7 +5,11 @@ import cn from 'classnames';
 import { throttle } from 'lodash';
 import * as types from '../../../constants/ActionTypes';
 import { formatTime } from '../../../utils/base';
-import { playerConf, PLAYER_MINIMIZE_TIMEOUT } from '../../../constants';
+import {
+  playerConf,
+  PLAYER_MINIMIZE_TIMEOUT,
+  MAX_VIDEO_SYNC_OFFSET,
+} from '../../../constants';
 import ProgressBar from './ProgressBar';
 import Subtitles from './Subtitles';
 import { fetchSubs } from '../../../actions';
@@ -71,7 +75,7 @@ function Player(props) {
   function checkDelay() {
     const { actualTime, currentTime } = props.media;
 
-    if (Math.abs(actualTime - currentTime) > 15) {
+    if (Math.abs(actualTime - currentTime) > MAX_VIDEO_SYNC_OFFSET) {
       playerRef.current.seekTo(actualTime);
     }
   }
