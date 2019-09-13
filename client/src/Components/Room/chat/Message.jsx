ea3bef10b1c4de5ab42b3450081de4725c145d34
bonanov <38221096+bonanov@users.noreply.github.com>
2019-09-14 01:00:48 +0300

    fixes

--- client/src/Components/Room/chat/Message.jsx
+++ client/src/Components/Room/chat/Message.jsx
@@ -4,58 +4,59 @@ import safelySetInnerHTML from '../../../utils/safelySetInnerHTML ';
 import parseMarkup from '../../../utils/markup';
 import playSound from '../../../utils/HandleSounds';
 import notifications from '../../../utils/notifications';
+import MiniProfile from './MiniProfile';
 
 class Message extends PureComponent {
-  shown = false;
+    shown = false;
 
-  getClassNames = classes => {
-      const { online } = classes;
-      let { highlight } = classes;
+    getClassNames = classes => {
+        const { online } = classes;
+        let { highlight } = classes;
 
-      const onlineClass = online ? 'online' : 'offline';
-      highlight = highlight ? 'highlight' : '';
+        const onlineClass = online ? 'online' : 'offline';
+        highlight = highlight ? 'highlight' : '';
 
-      return { className: `chat-message ${onlineClass} ${highlight}` };
-  };
+        return { className: `chat-message ${onlineClass} ${highlight}` };
+    };
 
-  getStyles = (image, color) => {
-      const backgroundImage = image ? `url(${image})` : '';
-      const backgroundColor = color;
+    getStyles = (image, color) => {
+        const backgroundImage = image ? `url(${image})` : '';
+        const backgroundColor = color;
 
-      return { backgroundImage, backgroundColor };
-  };
+        return { backgroundImage, backgroundColor };
+    };
 
-  pageIsVisible = () => document.visibilityState === 'visible';
+    pageIsVisible = () => document.visibilityState === 'visible';
 
-  handleSounds = highlight => {
-      if (this.shown) return;
-      if (highlight && !this.pageIsVisible()) {
-          playSound();
-      }
-  };
+    handleSounds = highlight => {
+        if (this.shown) return;
+        if (highlight && !this.pageIsVisible()) {
+            playSound();
+        }
+    };
 
-  handleNotification = (highlight, opts) => {
-      if (this.shown) return;
-      this.shown = true;
-      if (this.pageIsVisible()) return;
-      if (highlight) notifications.addReplies(opts);
-      else notifications.addUnread();
-  };
+    handleNotification = (highlight, opts) => {
+        if (this.shown) return;
+        this.shown = true;
+        if (this.pageIsVisible()) return;
+        if (highlight) notifications.addReplies(opts);
+        else notifications.addUnread();
+    };
 
-  render() {
-      const { online, color, image, highlight, body } = this.props;
-      const { name, emojiList } = this.props;
+    render() {
+        const { online, color, image, highlight, body } = this.props;
+        const { name, emojiList } = this.props;
 
-      // this.handleSounds(highlight);
-      this.handleNotification(highlight, { name, body, image });
+        // this.handleSounds(highlight);
+        this.handleNotification(highlight, { name, body, image });
 
-      const renderMessageArgs = {
-          ...this.getStyles(image, color),
-          ...this.getClassNames({ online, highlight }),
-          ...parseMarkup({ body, emojiList, name }),
-      };
-      return <RenderMessage {...renderMessageArgs} {...this.props} />;
-  }
+        const renderMessageArgs = {
+            ...this.getStyles(image, color),
+            ...this.getClassNames({ online, highlight }),
+            ...parseMarkup({ body, emojiList, name }),
+        };
+        return <RenderMessage {...renderMessageArgs} {...this.props} />
+    }
 }
 
 function mapStateToProps(state) {
@@ -68,12 +69,16 @@ const RenderMessage = props => {
     const { color, className, backgroundColor, backgroundImage } = props;
     const { _ref, id, name, tempBody } = props;
     const { renderHeader, hideHeader } = props;
-    const { handleProfile } = props;
+    const { handleProfile, onAvatarClick } = props;
     return (
         <div data-id={id} className={className}>
             {renderHeader && !hideHeader && (
                 <div className="chat-message_header">
-                    <div style={{ backgroundImage, backgroundColor }} className="chat-avatar" />
+                    <div 
+                        style={{ backgroundImage, backgroundColor }} 
+                        onClick={onAvatarClick}
+                        className="chat-avatar"
+                    />
                     <span
                         ref={_ref}
                         data-name={name}
