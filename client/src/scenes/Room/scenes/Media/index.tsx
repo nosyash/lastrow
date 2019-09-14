e79a2828fe12e6176111f9f5b9f02b227745155d
bonanov <38221096+bonanov@users.noreply.github.com>
2019-09-14 19:27:57 +0300

    black screen in full screen

--- client/src/scenes/Room/scenes/Media/index.tsx
+++ client/src/scenes/Room/scenes/Media/index.tsx
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Player from './Player';
 
 export default function VideoContainer({ videoRef }) {
+    const [fullscreen, setFullscreen] = useState(false);
+    const fs = useRef(false);
+    useEffect(() => {
+        window.addEventListener('fullscreenchange', changeFullscreen)
+    }, [])
+
+    function changeFullscreen() {
+        setFullscreen(!fs.current)
+        fs.current = !fs.current
+    }
+
     return (
-        <div ref={videoRef} id="video-container" className="video-container">
+        <div
+            ref={videoRef}
+            id="video-container"
+            style={{ background: fullscreen ? 'black' : '' }}
+            className="video-container">
             <Player />
         </div>
     );
+}
+
+function usePrevious(value) {
+    const ref = useRef();
+    useEffect(() => {
+        ref.current = value;
+    });
+    return ref.current;
 }
