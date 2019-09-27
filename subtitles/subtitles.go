63dd1924c2047f26688158272adf03b59ede350b
nosyash <kkomi.komii@gmail.com>
2019-09-27 13:35:57 +0300

    check subs file size

--- subtitles/subtitles.go
+++ subtitles/subtitles.go
@@ -8,14 +8,23 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 	"time"
 )
+
+const maxFileSize = 10 * 1024 * 1024 // 15mb
+
+var errFileSize = fmt.Errorf("Subtitles must be less than or equal %dmb", (maxFileSize / (1024 * 1024)))
 
 // CreateFromBytes create temp subs. file from raw source any (actually not) and save to .srt
 // return url path to a .srt file
 func CreateFromBytes(sub []byte, subType, fname, uploadPath string) (string, error) {
 	var fpath string
+
+	if len(sub) > maxFileSize {
+		return "", errFileSize
+	}
 
 	switch subType {
 	case "srt":
@@ -48,6 +57,24 @@ func CreateFromBytes(sub []byte, subType, fname, uploadPath string) (string, err
 
 // CreateFromURL same as CreateFromBytes but before we need download .srt file
 func CreateFromURL(url, fname, uploadPath string) (string, error) {
+	// Before download file, we need to check file extension after that.
+	// To preload headers and check Content-Length.
+	// If Content-length more than maxFileSize then we don't need to download a file.
+
+	if ext := filepath.Ext(url); ext != ".srt" {
+		return "", fmt.Errorf("Received unsupported %s sub. format", ext)
+	}
+
+	response, err := http.Head(url)
+	if err != nil {
+		return "", err
+	}
+
+	length, _ := strconv.Atoi(response.Header.Get("Content-Length"))
+	if length > maxFileSize {
+		return "", errFileSize
+	}
+
 	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(15*time.Second))
 	req, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
