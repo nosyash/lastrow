05495fdf8e4592afb554d9b55c75d95043e7e8da
nosyash <kkomi.komii@gmail.com>
2019-10-13 13:42:31 +0300

    fix

--- ws/register.go
+++ ws/register.go
@@ -36,7 +36,7 @@ func handleRegRequest(conn *websocket.Conn) (*user, string, error) {
 }
 
 func handleGuestRegister(conn *websocket.Conn, room, name, uuid string) (*user, string, error) {
-	if name != "" && len(name) > minGuestNameLength && len(name) < maxGuestNameLength && len(uuid) == 64 {
+	if name != "" && len(name) >= minGuestNameLength && len(name) <= maxGuestNameLength && len(uuid) == 64 {
 		return &user{
 				Conn:  conn,
 				Name:  name,
