950c6809e28da4c93adc0a3ad64984f87ba6ae09
nosyash <kkomi.komii@gmail.com>
2019-12-11 00:05:35 +0300

    fix

--- api/server.go
+++ api/server.go
@@ -119,7 +119,7 @@ func (server Server) acceptWebsocket(w http.ResponseWriter, r *http.Request) {
 func (server Server) logAndServe(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Host == server.originHost {
-			server.reqLogger.Printf("[%s] -> %s %s %s\n", r.RemoteAddr, r.Method, r.URL, r.UserAgent())
+			// server.reqLogger.Printf("[%s] -> %s %s %s\n", r.RemoteAddr, r.Method, r.URL, r.UserAgent())
 			handler.ServeHTTP(w, r)
 		}
 	})
